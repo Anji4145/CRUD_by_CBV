e25992fe9a1af865d29a7296501e60924ae6ad80
Anji4145 <anji15022@gmail.com>
2023-10-14 22:49:32 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class School(models.Model):
@@ -9,11 +11,16 @@ class School(models.Model):
 
     def __str__(self):
         return self.sname
+
+
+    def get_absolute_url(self):
+        return reverse('detail',kwargs ={'pk':self.pk})
     
 class Student(models.Model):
     stname = models.CharField(max_length = 20)
-    sage = models.IntegerField()
+    stage = models.IntegerField()
     sname = models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
 
     def __str__(self):
         return self.stname
+    
--- app/views.py
+++ app/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView,TemplateView,UpdateView,DeleteView
 from app.models import *
+from django.urls import reverse_lazy
+
+class home(TemplateView):
+    template_name = 'app/home.html' 
 
 class SchoolList(ListView):
-    model = School
+    model = School 
     context_object_name = 'schools'
+
+class SchoolDetail(DetailView):
+    model = School
+    context_object_name = 'schoolobj'
+
+class SchoolCreate(CreateView):
+    model = School
+    fields = '__all__'
+
+class SchoolUpdate(UpdateView):
+    model = School
+    fields = '__all__'
+
+class SchoolDelete(DeleteView):
+    model = School
+    context_object_name = 'schoolobject'
+    success_url = reverse_lazy('SchoolList')
